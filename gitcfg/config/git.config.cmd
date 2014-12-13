e83680d72d34b45d357b284ae56879ebcfd789b3
Kalin Toshev <kalint@microsoft.com>
2014-12-13 08:56:19 -0800

    Added b64 in tools; small changes in git shortcuts

--- gitcfg/config/git.config.cmd
+++ gitcfg/config/git.config.cmd
@@ -5,8 +5,9 @@ git config --system alias.hist "log --graph --decorate --oneline"
 git config --system alias.hista "log --graph --all --decorate --oneline"
 git config --system alias.dt "difftool -d"
 git config --system alias.dts "difftool -d --staged"
+git config --system alias.mr "merge --no-commit --no-ff"
 
-git config --system core.editor=gvim.exe
+git config --system core.editor gvim.exe
 git config --system diff.tool windiff
 git config --system difftool.windiff.cmd "windiff.exe \"$LOCAL\" \"$REMOTE\""
 git config --system difftool.prompt false
---
+++ tools/b64.ps1
@@ -0,0 +1,34 @@
+﻿function Base64Decode 
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [Alias("i")]
+        [string]$InputFile, 
+        [Parameter(Mandatory = $true)]
+        [Alias("o")]
+        [string]$OutputFile
+    )
+
+    $c = [System.IO.File]::ReadAllText($InputFile);
+
+    [System.IO.File]::WriteAllBytes($OutputFile, [System.Convert]::FromBase64String($c));
+}
+
+function Base64Encode
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [Alias("i")]
+        [string]$InputFile, 
+        [Parameter(Mandatory = $true)]
+        [Alias("o")]
+        [string]$OutputFile
+    )
+
+    $c = [System.IO.File]::ReadAllBytes($InputFile);
+
+    [System.IO.File]::WriteAllText($OutputFile, [System.Convert]::ToBase64String($c));
+}
+
