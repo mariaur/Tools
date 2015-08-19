d0d1674ddf9c0b61a49c10a1e7b1f81f960afe46
Kalin Toshev <kalint@microsoft.com>
2015-08-18 19:41:55 -0700

    gh tweaks

--- gitcfg/gh.cmd
+++ gitcfg/gh.cmd
@@ -1,1 +1,6 @@
-@git log --pretty="format:%%C(auto)%%h%%Creset%%C(auto)%%d%%Creset %%s %%C(cyan)[%%an]%%Creset %%Cgreen(%%ar)%%Creset" --graph %* .
+@echo off 
+if "%*" NEQ "" (
+    git log --pretty="format:%%C(auto)%%h%%Creset%%C(auto)%%d%%Creset %%s %%C(cyan)[%%ce]%%Creset %%Cgreen(%%ar)%%Creset" --graph %*
+) else (
+    git log --pretty="format:%%C(auto)%%h%%Creset%%C(auto)%%d%%Creset %%s %%C(cyan)[%%ce]%%Creset %%Cgreen(%%ar)%%Creset" --graph .
+)
