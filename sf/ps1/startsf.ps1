d996972a81ca539a63c0ca99304e0098f25aa0fa
Kalin Toshev <kalint@microsoft.com>
2015-11-04 13:11:22 -0800

    SF script fixes

--- sf/ps1/startsf.ps1
+++ sf/ps1/startsf.ps1
@@ -1,27 +1,27 @@
-﻿param([string]$_, [string]$nodeType = "SingleNodeType")
+﻿param([string]$_, [string]$nodeDomainType = "MT")
 
 # Source the init script
 . $PSScriptRoot\sfinit.ps1
 
 # Cluster manifest placeholder
-$nodeTypePlaceholder = '$$$SingleNodeType$$$'
+$nodeDomainTypePlaceholder = '$$$NodeDomainType$$$'
 
 # Get the node type (specialized) cluster manifest file name
-$clusterManifestForNodeType = Join-Path ([IO.Path]::GetTempPath()) (Split-Path $clusterManifest -Leaf)
+$clusterManifestForNodeDomainType = Join-Path ([IO.Path]::GetTempPath()) (Split-Path $clusterManifest -Leaf)
 
 Write-Host
-Write-Host "INFO: Creating cluster manifest for node type '$nodeType' ('$clusterManifest' -> '$clusterManifestForNodeType') ..."
+Write-Host "INFO: Creating cluster manifest for node domain type '$nodeDomainType' ('$clusterManifest' -> '$clusterManifestForNodeDomainType') ..."
 
-(Get-Content -Raw $clusterManifest).Replace($nodeTypePlaceholder, $nodeType) | Set-Content $clusterManifestForNodeType -Force
+(Get-Content -Raw $clusterManifest).Replace($nodeDomainTypePlaceholder, $nodeDomainType) | Set-Content $clusterManifestForNodeDomainType -Force
 
 # Check the cluster manifest
 Write-Host
-Write-Host "INFO: Checking cluster manifest ('$clusterManifestForNodeType') ..."
-Test-ServiceFabricClusterManifest -ClusterManifestPath "$clusterManifestForNodeType"
+Write-Host "INFO: Checking cluster manifest ('$clusterManifestForNodeDomainType') ..."
+Test-ServiceFabricClusterManifest -ClusterManifestPath "$clusterManifestForNodeDomainType"
 
 Write-Host
 Write-Host "INFO: Creating cluster node configuration ..."
-New-ServiceFabricNodeConfiguration -RunFabricHostServiceAsManual -ClusterManifestPath $clusterManifestForNodeType `
+New-ServiceFabricNodeConfiguration -RunFabricHostServiceAsManual -ClusterManifestPath $clusterManifestForNodeDomainType `
     -FabricDataRoot $fabricDataRoot -FabricLogRoot $fabricLogRoot -Verbose 
 
 # Start the Service Fabric host
