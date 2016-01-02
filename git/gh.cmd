268ebb6239479870f7665c154d59ab2c58e95412
Kalin Toshev <kalin.toshev@gmail.com>
2016-01-02 14:57:35 -0800

    Improvements in gh command

--- git/gh.cmd
+++ git/gh.cmd
@@ -1,6 +1,1 @@
-@echo off 
-if {%1} NEQ {} (
-    git log --pretty="format:%%C(auto)%%h%%Creset%%C(auto)%%d%%Creset %%s %%C(cyan)[%%ce]%%Creset %%Cgreen(%%ar)%%Creset" --graph %*
-) else (
-    git log --pretty="format:%%C(auto)%%h%%Creset%%C(auto)%%d%%Creset %%s %%C(cyan)[%%ce]%%Creset %%Cgreen(%%ar)%%Creset" --graph .
-)
+@powershell %~dp0\gh.ps1 '%*'
---
+++ git/gh.ps1
@@ -0,0 +1,17 @@
+param($a)
+
+$isRedir = [System.Console]::IsOutputRedirected
+
+if ($isRedir)
+{
+    $format = 'format:%h%d %s [%ce] (%ar)'
+}
+else
+{
+    $format = 'format:%C(auto)%h%Creset%C(auto)%d%Creset %s %C(cyan)[%ce]%Creset %Cgreen(%ar)%Creset'
+    $graph = '--graph'
+}
+
+$cmd = 'git log --pretty="' + $format + '" ' + $graph + ' ' + $a + ' .'
+iex $cmd
+
