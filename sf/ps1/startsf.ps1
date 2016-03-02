ba7d8ec5c398785c2fb5684cb4de5315d10ddcab
Kalin Toshev <kalint@microsoft.com>
2016-03-01 23:55:46 -0800

    Extended the cluster manifest to FE, MT

--- sf/ps1/startsf.ps1
+++ sf/ps1/startsf.ps1
@@ -1,27 +1,14 @@
-﻿param([string]$_, [string]$nodeDomainType = "MT")
-
-# Source the init script
+﻿# Source the init script
 . $PSScriptRoot\sfinit.ps1
-
-# Cluster manifest placeholder
-$nodeDomainTypePlaceholder = '$$$NodeDomainType$$$'
-
-# Get the node type (specialized) cluster manifest file name
-$clusterManifestForNodeDomainType = Join-Path ([IO.Path]::GetTempPath()) (Split-Path $clusterManifest -Leaf)
-
-Write-Host
-Write-Host "INFO: Creating cluster manifest for node domain type '$nodeDomainType' ('$clusterManifest' -> '$clusterManifestForNodeDomainType') ..."
-
-(Get-Content -Raw $clusterManifest).Replace($nodeDomainTypePlaceholder, $nodeDomainType) | Set-Content $clusterManifestForNodeDomainType -Force
 
 # Check the cluster manifest
 Write-Host
-Write-Host "INFO: Checking cluster manifest ('$clusterManifestForNodeDomainType') ..."
-Test-ServiceFabricClusterManifest -ClusterManifestPath "$clusterManifestForNodeDomainType"
+Write-Host "INFO: Checking cluster manifest ($clusterManifest) ..."
+Test-ServiceFabricClusterManifest -ClusterManifestPath $clusterManifest | Out-Null
 
 Write-Host
 Write-Host "INFO: Creating cluster node configuration ..."
-New-ServiceFabricNodeConfiguration -RunFabricHostServiceAsManual -ClusterManifestPath $clusterManifestForNodeDomainType `
+New-ServiceFabricNodeConfiguration -RunFabricHostServiceAsManual -ClusterManifestPath $clusterManifest `
     -FabricDataRoot $fabricDataRoot -FabricLogRoot $fabricLogRoot -Verbose 
 
 # Start the Service Fabric host
