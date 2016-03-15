3d0afaa4f0053bcceaa919f54975095ee36ea008
Anton Nepomnyashchiy <antonn@microsoft.com>
2016-03-15 14:13:39 -0700

    Add param support the Deploy

--- git/WDGit.env.cmd
+++ git/WDGit.env.cmd
@@ -5,7 +5,7 @@ doskey gs=git status
 doskey pull=git pull
 doskey push=git push
 doskey cp=pushd C:\Git\WD.Services.CloudProtection
-doskey prepush=call C:\Git\validate.cmd
-doskey deploy=powershell "C:\Git\WD.Services.CloudProtection\Scripts\Redeploy-CloudProtectionToDevCluster.ps1"
+doskey prepush=call C:\Git\Tools\git\validate.cmd
+doskey deploy=powershell "C:\Git\WD.Services.CloudProtection\Scripts\Redeploy-CloudProtectionToDevCluster.ps1" $*
 rem start VS 2015.1 Prompt
 "%VS140COMNTOOLS%\VsDevCmd.bat" amd64
