7d5af70d905caafe3abd57ade24688e2438db39b
Anton Nepomnyashchiy <antonn@microsoft.com>
2016-09-19 14:23:59 -0700

    Rename to 'My' folder + consume relative paths

--- My/WDGit.env.cmd
+++ My/WDGit.env.cmd
@@ -5,11 +5,10 @@ doskey GS=git status
 doskey Pull=git pull
 doskey Push=git push
 doskey cdcp=pushd C:\Git\WD.Services.CloudProtection
-doskey StashPull=call C:\Git\Tools\WDGit\StashPull.cmd
-doskey Validate=call C:\Git\Tools\WDGit\Validate.cmd
+doskey StashPull=call "%~dp0\StashPull.cmd"
+doskey Validate=call "%~dp0\Validate.cmd"
 doskey StartSF=powershell "C:\Git\WD.Services.CloudProtection\Scripts\Restart-ServiceFabricDevCluster.ps1 $*"
 doskey DeployCP=powershell "C:\Git\WD.Services.CloudProtection\Scripts\Redeploy-CloudProtectionToDevCluster.ps1 CloudEngine $*"
-doskey PushApp=call C:\Git\Tools\WDGit\PushAppPPE.cmd
 rem start VS 2015.2 Prompt
 "%VS140COMNTOOLS%\VsDevCmd.bat" amd64
-call C:\Git\Tools\WDGit\GitDiffSetup.cmd
+call '%~dp0\GitDiffSetup.cmd'
