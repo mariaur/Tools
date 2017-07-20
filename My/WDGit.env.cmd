b5bfd48d290a7c80c946c33d08da318a30904313
Anton Nepomnyashchiy <antonn@microsoft.com>
2017-07-20 15:56:10 -0700

    Update NuGet

--- My/WDGit.env.cmd
+++ My/WDGit.env.cmd
@@ -3,7 +3,9 @@ title Git CMD
 doskey NP="C:\Program Files (x86)\Notepad++\notepad++.exe" $*
 doskey GS=git status
 doskey Pull=git pull --rebase
-doskey Push=git push
+doskey Push=git push $*
+doskey co=git checkout "$*"
+doskey nb=git checkout -b "$*"
 doskey ..=cd ..
 rem start VS 2015.2 Prompt
 "%VS140COMNTOOLS%\VsDevCmd.bat" amd64
